46fd3c272f6e916b377bba45803ed440644bfea4
Real Ivan Nosov <i-am-ivan-nosov@yandex.com>
2022-02-07 21:31:58 +0300

    add files buffers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-'ku'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App/App';
+import { BookstoreServiceProvider } from './components/BookstoreServiceContext/BookstoreServiceContext';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+
+
+ReactDOM.render('ku', document.getElementById('root'));
